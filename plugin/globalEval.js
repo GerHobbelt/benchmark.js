dc2dbc55a628b1d8f426dd885833b6fd0a524209
Ger Hobbelt <ger@hobbelt.com>
2016-09-01 18:02:16 +0200

    we will need globalEval for NodeJS as well!

--- plugin/globalEval.js
+++ plugin/globalEval.js
@@ -1,30 +1,28 @@
 // http://perfectionkills.com/global-eval-what-are-the-options/#evaling_in_global_scope
 
-var globalEval = (function() {
+var globalEval = (function () {
 
-  var isIndirectEvalGlobal = (function(original, Object) {
+  var isIndirectEvalGlobal = (function (original, Object) {
     try {
       // Does `Object` resolve to a local variable, or to a global, built-in `Object`,
       // reference to which we passed as a first argument?
-      return (1,eval)('Object') === original;
+      return (1, eval)('Object') === original;
     }
-    catch(err) {
+    catch (err) {
       // if indirect eval errors out (as allowed per ES3), then just bail out with `false`
       return false;
     }
   })(Object, 123);
 
   if (isIndirectEvalGlobal) {
-
     // if indirect eval executes code globally, use it
-    return function(expression) {
-      return (1,eval)(expression);
+    return function (expression) {
+      return (1, eval)(expression);
     };
   }
-  else if (typeof window.execScript !== 'undefined') {
-
+  else if (typeof window !== 'undefined' && typeof window.execScript !== 'undefined') {
     // if `window.execScript exists`, use it
-    return function(expression) {
+    return function (expression) {
       return window.execScript(expression);
     };
   }
