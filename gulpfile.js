01becd7cd24a2704cb765e71ebc15a4c77db144b
Ger Hobbelt <ger@hobbelt.com>
2016-09-29 20:57:43 +0200

    completed work on gulp tasks to produce a quick website for this benchmark.js clone.

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,10 @@ var less = require('gulp-less');
 var LessAutoprefix = require('less-plugin-autoprefix');
 var stripCssComments = require('gulp-strip-css-comments');
 var MarkdownIt = require('markdown-it');
+var gutil = require('gulp-util');
 var fncallback = require('gulp-fncallback');
 var path = require('path');
+var fs = require('fs');
 
 var autoprefix = new LessAutoprefix({ browsers: ['last 2 versions'] });
 
@@ -74,7 +76,9 @@ gulp.task('patch-version', function () {
 gulp.task('gh-pages', function () {
   var md = new MarkdownIt();
 
-  return gulp.src(['./*.md', './doc/*.md'], {base: './'})
+  var tpl = fs.readFileSync('website-assets/html/index.html.template', 'utf8');
+
+  return gulp.src(['./README.md', './doc/README.md'], {base: './'})
     .pipe(fncallback(function (file, enc, cb) {
       var fn = file.relative;
 
@@ -82,18 +86,22 @@ gulp.task('gh-pages', function () {
 
       var result = md.render(content);
       
+      file.path = file.path.replace('README.md', 'index.html');
+
+      console.log('path = ', fn, fn.replace(/\\/g, '/'), fn.replace(/\\/g, '/').match(/\//g));
+      var p = fn.replace(/\\/g, '/').match(/\//g) || [];
+      var dirdepth = p.length;
+      var tpl_vars = {
+        cloner: 'GerHobbelt',
+        author: 'BestieJS',
+        file: file,
+        content: result, 
+        basedir: ['.', '..', '../..', '../../..'][dirdepth],
+      };
+      result = gutil.template(tpl /* 'test <%= name %> <%= file.path %>' */, tpl_vars);
+
       file.contents = new Buffer(result);
 
-      file.path = gutil.replaceExtension(file.path, '.html');
-
-      if (0) {
-        var opt = {
-          name: 'todd',
-          file: someGulpFile
-        };
-        gutil.template('test <%= name %> <%= file.path %>', opt);
-      }                                                                
-      
       cb();
     }))
     .pipe(gulp.dest('./'));
