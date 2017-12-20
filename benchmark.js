16ec58b4eeafa5a2f79b464960c837f7c754c191
Ger Hobbelt <ger@hobbelt.com>
2017-12-20 23:56:41 +0100

    bumped build revision

--- benchmark.js
+++ benchmark.js
@@ -1,5 +1,5 @@
 /*!
- * Benchmark.js <https://benchmarkjs.com/> 2.1.4-28
+ * Benchmark.js <https://benchmarkjs.com/> 2.1.4-29
  * Copyright 2010-2016 Mathias Bynens <https://mths.be/>
  * Based on JSLitmus.js, copyright Robert Kieffer <http://broofa.com/>
  * Modified by John-David Dalton <http://allyoucanleet.com/>
@@ -2541,7 +2541,7 @@
        * @memberOf Benchmark
        * @type string
        */
-      version: '2.1.4-28'
+      version: '2.1.4-29'
     });
 
     _.assign(Benchmark, {
